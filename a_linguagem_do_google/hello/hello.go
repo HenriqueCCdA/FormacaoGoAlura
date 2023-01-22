5699cc4687f92e9a3ddc6a232c8783393d40c443
Henrique Andrade <henrique.ccda@gmail.com>
2023-01-22 18:40:10 -0300

    Fim da aula 5

--- a_linguagem_do_google/hello/hello.go
+++ a_linguagem_do_google/hello/hello.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	comando := lerComando()
+		comando := lerComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -43,6 +50,33 @@ func lerComando() int {
 	var comandoLido int
 
 	fmt.Scan(&comandoLido)
+	fmt.Println("")
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+
+	sites := []string{"https://httpstat.us/200", "https://httpstat.us/404", "https://www.alura.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for j, site := range sites {
+			fmt.Println("Testano site", j, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+	}
 }
