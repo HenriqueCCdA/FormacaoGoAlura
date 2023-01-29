b534153fe1fa5f2450dcf7096986be5e53296b7e
Henrique Andrade <henrique.ccda@gmail.com>
2023-01-29 16:09:35 -0300

    Fim do curso1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-1. Curso de Go: a linguagem do Google: Inicio: 17/01/2023 Fim: --/--/2023
+1. Curso de Go: a linguagem do Google: Inicio: 17/01/2023 Fim: 29/01/2023
+2. Curso de Go: Orientação a Objetos: 29/01/2023 Fim: --/--/2023
--- a_linguagem_do_google/hello/hello.go
+++ a_linguagem_do_google/hello/hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,7 +27,7 @@ func main() {
 		case 1:
 			iniciarMonitoramento()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			os.Exit(0)
 		default:
@@ -86,8 +88,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -115,4 +119,28 @@ func leSiteDoArquivo() []string {
 	}
 
 	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + "-" + site + "- online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
 }
