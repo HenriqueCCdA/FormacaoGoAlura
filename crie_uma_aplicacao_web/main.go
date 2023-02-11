9a081d7d36c0a4ee263579539390f88f646fae96
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-10 23:35:29 -0300

    Curso 3 aula 1

---
+++ crie_uma_aplicacao_web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	produtos := []Produto{
+		{"Camiseta", "Azul, bem bonita", 39, 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 2},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
