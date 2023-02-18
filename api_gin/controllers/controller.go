aac9d039783abed94945a1975dfb70795e28b572
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-18 18:27:27 -0300

    curso 5 aula 3

--- api_gin/controllers/controller.go
+++ api_gin/controllers/controller.go
@@ -1,13 +1,19 @@
 package controllers
 
 import (
+	"api-rest-gin/database"
 	"api-rest-gin/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, models.Alunos)
+	var alunos []models.Aluno
+
+	result := database.DB.Find(&alunos)
+
+	c.JSON(200, result)
 }
 
 func Saudacao(c *gin.Context) {
@@ -16,4 +22,17 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- api_gin/main.go
+++ api_gin/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"api-rest-gin/models"
+	"api-rest-gin/database"
 	"api-rest-gin/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno{
-		{Nome: "Gui Lima", CPF: "32132132111", RG: "47000000000"},
-		{Nome: "Ana", CPF: "12312312322", RG: "48000000000"},
-	}
+	database.ConectaComBancoDeDados()
+
 	routes.HandleRequests()
 }
